3afbf0d0834e2e1f1d6e5759f1e5a30d7cea03e9
mostafaSystemha <m.mohamed@systemha.com>
2024-01-01 01:30:04 +0200

    update on migration

--- database/migrations/2023_12_20_000610_create_sliders_table.php
+++ database/migrations/2023_12_20_000610_create_sliders_table.php
@@ -15,6 +15,10 @@ class CreateSlidersTable extends Migration
     {
         Schema::create('sliders', function (Blueprint $table) {
             $table->id();
+            $table->string('ar_title');
+            $table->string('en_title');
+            $table->string('ar_subtitle');
+            $table->string('en_subtitle');
             $table->timestamps();
         });
     }
